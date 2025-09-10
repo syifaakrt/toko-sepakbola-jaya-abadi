f0df093e1a25772b93801b211cad45ad9f09f510
Syifa Anabella <syifaakrt@gmail.com>
2025-09-10 17:36:48 +0700

    fix error

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ class Product (models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField
     description = models.TextField(max_length=500)
-    thumbnail = models.URLField(_(""), max_length=200)
+    thumbnail = models.URLField((""), max_length=200)
     category = models.CharField
     is_featured = models.BooleanField
