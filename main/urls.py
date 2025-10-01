9bc93e751547d51b168d2bf35b331bfa3b80e7dc
Syifa <syifaakrt@gmail.com>
2025-10-01 08:08:34 +0700

    implement css

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_json, show_json_byId, show_xml, show_xml_byId, add_product, show_product, register, login_user, logout_user
+from main.views import show_main, show_json, show_json_byId, show_xml, show_xml_byId, add_product, show_product, register, login_user, logout_user, edit_product, delete_product
 
 app_name = 'main'
 
@@ -13,5 +13,7 @@ urlpatterns = [
     path('product/<str:productId>/', show_product, name = 'show_product'),
     path('register/', register, name = 'register'),
     path('login/', login_user, name = 'login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('product/<uuid:productId>/edit', edit_product, name='edit_product'),
+    path('product/<uuid:producId>/delete', delete_product, name='delete_product')
 ]
--- main/views.py
+++ main/views.py
@@ -92,3 +92,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, productId):
+    product = get_object_or_404(Product, pk=productId)
+    form = ProductForms(request.POST or None, instance=product)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, productId):
+    product = get_object_or_404(Product, pk=productId)
+    product.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
